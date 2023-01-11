56d154cc9bf1ea6e60bdb4b95dbf79527e5c61c8
Joseph Emrick <josephemrick@Josephs-MacBook-Air.local>
2023-01-10 18:11:37 -0800

    css changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,14 @@
+import { SocialIcon } from 'react-social-icons'
+
 function Footer(props) {
     return <>
-    
+    <footer className='footer'>
+    <div className='contacts'>
+    <SocialIcon url="https://linkedin.com/in/joe-emrick" />
+    <SocialIcon url="https://github.com/Jcemrick"/>
+    <SocialIcon url="mailto:jcemrick@gmail.com"/>
+    </div>
+    </footer>
     </>
 }
 
--- src/pages/About.js
+++ src/pages/About.js
@@ -6,9 +6,20 @@ function About(props) {
 
     return <>
     <div className='aboutcontainer'>
-        <h2>{about.name}</h2>
-        <h3>{about.email}</h3>
-        <p>{about.bio}</p>
+        <div className='card'>
+            <img src={about.cardshot}/>
+            <h3>{about.name}</h3>
+            <h4>Level: {about.level}</h4>
+            <p>{about.bio}</p>
+            <h4>Stats:</h4>
+            <ul>
+                <li>HP: {about.hp}</li>
+                <li>Stamina: {about.stamina}</li>
+                <li>Magic: {about.magic}</li>
+                <li>Strength: {about.strength}</li>
+                <li>Weakness: {about.weakness}</li>
+            </ul>
+        </div>
     </div>
     </>
 }
